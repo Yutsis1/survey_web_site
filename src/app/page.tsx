4aede5c9a5bb03e67f8b91d4f3912db88598bfb8
Roman Iutsis <yutsis5@egmail.com>
2025-01-29 22:20:35 +0100

    Added the radiobutton bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Button } from "./components/button/button";
 import { Checkbox } from "./components/checkbox/checkbox";
-import { Radio } from "./components/radios/radio-component";
+import { RadioBar } from "./components/radios/radio-bar";
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
         <Button label={"lol"} className="button-base right"/>
         <p>kek</p>
         <Checkbox activeLabel="ON" inactiveLabel="OFF" className="checkbox-wrapper"/>
-        <Radio label={"lol"} className="radio-wrapper"/>
+        <RadioBar buttons={[{label: "kek", value: "kek"}, {label: "lol", value: "lol"}]} name="kek"/>
       </div>
     </>
   );
