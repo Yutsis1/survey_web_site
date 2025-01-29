53f8283e2ffcaad30a9bcf415c892646fc978519
Roman Iutsis <yutsis5@egmail.com>
2025-01-29 22:20:35 +0100

    Added the radiobutton bar

---
+++ src/app/components/radios/radio-bar.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import React, { useState } from "react";
+import "./radio-bar.css";
+
+interface RadioProps {
+  label: string;
+  value: string;
+}
+
+interface RadioBarProps {
+  buttons: RadioProps[];
+  name: string;
+  test_id?: string;
+}
+
+const RadioBar: React.FC<RadioBarProps> = ({ buttons, name, test_id }) => {
+  const [selectedValue, setSelectedValue] = useState<string | null>(null);
+
+  const handleChange = (value: string) => {
+    setSelectedValue(value);
+  };
+
+  return (
+    <div className="radio-bar" data-testid={test_id}>
+      {buttons.map((button, index) => (
+        <label key={index} className="radio-wrapper">
+          <input
+            type="radio"
+            name={name}
+            value={button.value}
+            checked={selectedValue === button.value}
+            onChange={() => handleChange(button.value)}
+          />
+          <span className="radio-label">{button.label}</span>
+        </label>
+      ))}
+    </div>
+  );
+};
+
+export { RadioBar };
