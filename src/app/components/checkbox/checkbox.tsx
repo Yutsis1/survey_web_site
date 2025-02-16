2881481810bc9efc1edb896e5a7dfc3b016a2015
Roman Iutsis <yutsis5@egmail.com>
2025-02-16 14:53:15 +0100

    Update next. REwork toggles

--- src/app/components/checkbox/checkbox.tsx
+++ src/app/components/checkbox/checkbox.tsx
@@ -1,38 +1,25 @@
-'use client'
+"use client";
 
 import React, { useState } from "react";
-import "./checkbox.css"
+import "./checkbox.css";
 
-interface CheckboxProps {
-  activeLabel?: string;
-  inactiveLabel?: string;
-  test_id?: string;
-  className?: string;
+interface ToggleSwitchProps {
+  initialState?: boolean;
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({ activeLabel, inactiveLabel, className, test_id }) => { 
-  const [isChecked, setIsChecked] = useState(false);
-  const test_id_internal = test_id ? test_id : Math.random().toString(36).substring(7);
-  const id = `${test_id_internal}_${activeLabel}`;
-  
+const Checkbox: React.FC<ToggleSwitchProps> = ({ initialState = false }) => {
+  const [isChecked, setIsChecked] = useState(initialState);
 
   return (
-    <div className={`checkbox-wrapper ${className || ''}`}>
-      <div className='block'>
-        <input 
-          type="checkbox" 
-          data-index="0" 
-          data-testid={test_id} 
-          id={id}
-          checked={isChecked}
-          onChange={(e) => setIsChecked(e.target.checked)}
-        />
-        <label htmlFor={id}></label>
-          <span className="checkbox-label">
-            {isChecked ? activeLabel : inactiveLabel}
-          </span>
-      </div>
-    </div>
+    <label className="toggle-switch">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <span className="slider"></span>
+      <span className="label">{isChecked ? "Active" : "Inactive"}</span>
+    </label>
   );
 };
 
