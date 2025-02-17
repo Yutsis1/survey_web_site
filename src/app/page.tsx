9e08bd983f61ceb1307ffe88b18d9f0ba1bd0ebf
Roman Iutsis <yutsis5@egmail.com>
2025-02-17 18:44:01 +0100

    Added support of grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,38 @@
 "use client";
-import Image from "next/image";
-import { Button } from "./components/button/button";
-import { Checkbox } from "./components/checkbox/checkbox";
-import { RadioBar } from "./components/radios/radio-bar";
-import { TextInput } from "./components/text-field/text-field";
 import { useState } from "react";
 import { Question } from "./app-modules/questions/question";
+import { Responsive, WidthProvider } from "react-grid-layout";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export default function Home() {
   const [text, setText] = useState("");
 
+  const layout = [
+    { i: "question", x: 0, y: 0, w: 2, h: 2 },
+  ];
+
   return (
-    <>
-      <a>Hello</a>
-      <div>
-        <Button label={"kek"} className="button-base" />
-        <Button label={"lol"} className="button-base right" />
-        <p>kek</p>
-        <Checkbox activeLabel="ON" inactiveLabel="OFF" />
-        <RadioBar
-          buttons={[
-            { label: "kek", value: "kek" },
-            { label: "lol", value: "lol" },
-          ]}
-          name="kek"
-        />
-        <TextInput
-          label="Your Name"
-          placeholder="Enter your name"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <p>Typed Value: {text}</p>
-        <Question
-          questionText="KEK"
-          component="Checkbox"
-          options={[
-            { optionProps: { activeLabel: "ON", inactiveLabel: "OFF" } },
-          ]}
-        />
-      </div>
+    <>  
+        <ResponsiveGridLayout
+          className="layout"
+          layouts={{ lg: layout }}
+          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+          rowHeight={30}
+        >
+          <div key="question">
+            <Question
+              questionText="KEK"
+              component="Checkbox"
+              options={[
+                { optionProps: { activeLabel: "ON", inactiveLabel: "OFF" } },
+              ]}
+            />
+          </div>
+        </ResponsiveGridLayout>
     </>
   );
 }
