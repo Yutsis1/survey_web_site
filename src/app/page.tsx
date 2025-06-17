d7b7f7d71b2e51aa94fe8ae24af89ceae60fc065
Roman Iutsis <yutsis5@egmail.com>
2025-06-17 11:08:19 +0200

    feat: update styling for radio buttons and clean up unused CSS in globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,117 @@
-"use client";
-import { useState } from "react";
-import { GridElement } from "./app-modules/grid-element/grid-element";
+'use client'
+import { useState } from 'react'
+import { GridElement } from './app-modules/grid-element/grid-element'
 // import { Responsive, WidthProvider } from "react-grid-layout";
-import "react-grid-layout/css/styles.css";
-import "react-resizable/css/styles.css";
-import "./styles.css"; // Import your styles
-import { Sidebar } from "./app-modules/sidebar/sidebar";
-import { PopUp } from "./app-modules/pop-up/pop-up";
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
+import './styles.css' // Import your styles
+import { Sidebar } from './app-modules/sidebar/sidebar'
+import { PopUp } from './app-modules/pop-up/pop-up'
+import { componentMapping } from './app-modules/interfaceMapping'
 
 export default function Home() {
-  const [isChecked, setIsChecked] = useState(false);
-  const [isDragging, setIsDragging] = useState(false); // Track drag state
-  const [isPopUpOpen, setIsPopUpOpen] = useState(false); // Add popup state
+    const [isChecked, setIsChecked] = useState(false)
+    const [isDragging, setIsDragging] = useState(false) // Track drag state
+    const [isPopUpOpen, setIsPopUpOpen] = useState(false) // Add popup state
 
+
+    // test implementation of buttons based on components structure 
+    // const buttonProps = Object.keys(componentMapping).map(
+    //     (componentKey, index) => ({
+    //         label: `New ${componentKey}`,
+    //         onClick: () => {
+    //             console.log(`New ${componentKey} clicked`)
+    //             setIsPopUpOpen(true)
+    //         },
+    //         className: 'button-base',
+    //         test_id: `button-${index + 1}`,
+    //     })
+    // )
 
     const handlePopUpApply = () => {
-    // Add your apply logic here
-    console.log("Apply clicked");
-    setIsPopUpOpen(false); // Close popup after applying
-  };
+        // Add your apply logic here
+        console.log('Apply clicked')
+        setIsPopUpOpen(false) // Close popup after applying
+    }
 
-  const handlePopUpClose = () => {
-    setIsPopUpOpen(false);
-  };
+    const handlePopUpClose = () => {
+        setIsPopUpOpen(false)
+    }
 
-  const buttonProps = [
-    {
-      label: "New Question",
-      onClick: () => {
-        console.log("New Question clicked");
-        setIsPopUpOpen(true); 
-      },
-      className: "button-base",
-      test_id: "button-1",
-    },
-    {
-      label: "Button 2",
-      onClick: () => {
-        console.log("Button 2 clicked");
-      },
-      className: "button-base",
-      test_id: "button-2",
-    },
-  ];
+    const buttonProps = [
+        {
+            label: 'New Question',
+            onClick: () => {
+                console.log('New Question clicked')
+                setIsPopUpOpen(true)
+            },
+            className: 'button-base',
+            test_id: 'button-1',
+        },
+        {
+            label: 'Button 2',
+            onClick: () => {
+                console.log('Button 2 clicked')
+            },
+            className: 'button-base',
+            test_id: 'button-2',
+        },
+    ]
 
-  return (
-    <div className="app-container">
-      <aside className="sidebar">
-        <Sidebar buttons={buttonProps}></Sidebar>
-      </aside>
-      <main className="content">
-        <div className="grid-container">
-          <GridElement
-            questionProps={{
-              questionText: "KEK",
-              component: "Checkbox",
-              option: 
-                {
-                  optionProps: {
-                    activeLabel: "ON",
-                    inactiveLabel: "OFF",
-                    checked: isChecked,
-                    onChange: (
-                      checked: boolean | ((prevState: boolean) => boolean)
-                    ) => {
-                      if (!isDragging) {
-                        setIsChecked(checked);
-                      }
+    return (
+        <div className="app-container">
+            <aside className="sidebar">
+                <Sidebar buttons={buttonProps}></Sidebar>
+            </aside>
+            <main className="content">
+                <div className="grid-container">
+                    <GridElement
+                        questionProps={{
+                            questionText: 'KEK',
+                            component: 'Checkbox',
+                            option: {
+                                optionProps: {
+                                    activeLabel: 'ON',
+                                    inactiveLabel: 'OFF',
+                                    checked: isChecked,
+                                    onChange: (
+                                        checked:
+                                            | boolean
+                                            | ((prevState: boolean) => boolean)
+                                    ) => {
+                                        if (!isDragging) {
+                                            setIsChecked(checked)
+                                        }
+                                    },
+                                },
+                            },
+                        }}
+                    />
+                </div>
+            </main>
+            {/* Add the PopUp component */}
+            <PopUp
+                isOpen={isPopUpOpen}
+                onClose={handlePopUpClose}
+                onApply={handlePopUpApply}
+                popUpTitle="Create New Question"
+                popUpDescription="Configure your new question settings below"
+                components={['RadioBar']}
+                options={[
+                    {
+                        optionProps: {
+                            buttons: Object.keys(componentMapping).map(
+                                (key) => ({
+                                    label: key,
+                                    value: key,
+                                })
+                            ),
+                            name: 'Which question type do you want to create?',
+                            test_id: 'radio-bar-question-type',
+                        },
                     },
-                  },
-                },
-              
-            }}
-          />
+                ]}
+            />
         </div>
-      </main>
-      {/* Add the PopUp component */}
-      <PopUp
-        isOpen={isPopUpOpen}
-        onClose={handlePopUpClose}
-        onApply={handlePopUpApply}
-        popUpTitle="Create New Question"
-        popUpDescription="Configure your new question settings below"
-        // Add components and options if needed
-        // components={["Checkbox"]}
-        // options={[{ optionProps: { /* your option props */ } }]}
-      />
-    </div>
-  );
+    )
 }
