ce0e596f5a2e7f42e0e29d21dc7b9e9256353308
Roman <yutsis5@gmail.com>
2025-06-19 14:52:47 +0200

    fix: update Jest configuration for TypeScript JSX handling; create separate tsconfig for tests (#11)
    Co-authored-by: Roman Iutsis <yutsis5@egmail.com>

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx'
+      }
+    }],
   },
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
